cff15cbfa198d6b16f0e87b4f43a91200ad23211
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-08-29 22:23:51 -0300

    ci: remove vit plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
 })
