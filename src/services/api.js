f824d6b73c835c0a5474faedd3af112d44f639db
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-09-01 17:37:33 -0300

    chore: add header authorization on endpoint route

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,18 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 })
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => {
+    // Handle error
+  },
+)
