3a910304099c0d74c6f334795aee8f809f70d935
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-09-04 00:00:52 -0300

    fix: remove work around :boom:

--- src/services/api.js
+++ src/services/api.js
@@ -4,17 +4,6 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 })
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-
-    return config
-  },
-  (error) => {
-    // Handle error
-  },
-)
+export const imageApi = axios.create({
+  baseURL: import.meta.env.VITE_IMAGE_API_URL,
+})
