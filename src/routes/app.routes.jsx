9124366e8752b0c19bd55f7e9db3704f52f94f15
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-09-05 20:05:22 -0300

    feat: create a cart context v2

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { useAuth } from '../hooks/auth'
 import { Details } from '../pages/Details'
 import { Home } from '../pages/Home'
 import { NewDish } from '../pages/NewDish'
@@ -6,13 +7,14 @@ import { UpdateDish } from '../pages/UpdateDish'
 import { DefaultLayout } from './Layouts'
 
 export function AppRoutes() {
+  const { isAdmin } = useAuth()
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/dish/:id" element={<Details />} />
-        <Route path="/new" element={<NewDish />} />
-        <Route path="/update/:id" element={<UpdateDish />} />
+        {isAdmin && <Route path="/new" element={<NewDish />} />}
+        {isAdmin && <Route path="/update/:id" element={<UpdateDish />} />}
       </Route>
     </Routes>
   )
