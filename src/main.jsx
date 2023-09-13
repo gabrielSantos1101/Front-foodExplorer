216a0f90fe2f7fc8272eada054a433beb9a62545
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-09-13 12:23:17 -0300

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
@@ -25,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           </CartProvider>
         </SearchProvider>
       </AuthProvider>
+      <Analytics />
       <ToastContainer
         position="top-right"
         autoClose={4000}
