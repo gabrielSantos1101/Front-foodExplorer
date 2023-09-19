2cc868ad55733cded1378d84dc435ff6b0d8c018
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-09-19 15:20:20 -0300

    refactor(Button): forward ref to fix warning on modal component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,24 +1,29 @@
+import { forwardRef } from 'react'
 import { Wrap } from './styles'
 
-export function Button({
-  title,
-  isText = false,
-  isCenter = false,
-  onClick,
-  hasIcon = false,
-  icon: Icon,
-  ...rest
-}) {
+export const Button = forwardRef(function Button(
+  {
+    title,
+    isText = false,
+    isCenter = false,
+    onClick,
+    hasIcon = false,
+    icon: Icon,
+    ...rest
+  },
+  ref,
+) {
   return (
     <Wrap
       $istext={isText}
       $iscenter={isCenter}
       onClick={onClick}
       $hasicon={hasIcon}
+      ref={ref}
       {...rest}
     >
       {hasIcon && <Icon />}
       {title}
     </Wrap>
   )
-}
+})
